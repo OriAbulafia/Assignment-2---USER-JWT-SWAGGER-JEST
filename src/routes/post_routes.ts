9a691b2927e7d31c8b028a8e3a4cd95473bdb875
Sahar <96saharyosef@gmail.com>
2024-12-06 13:19:48 +0200

    another update

--- src/routes/post_routes.ts
+++ src/routes/post_routes.ts
@@ -6,13 +6,14 @@ import {
   getPostById,
   deletePost,
 } from "../controllers/post_controller.js";
+import { authUser } from "../middleware/authMiddleware.js";
 
 const router: Router = express.Router();
 
-router.route("/").post(createPost);
+router.route("/").post(authUser, createPost);
 router.route("/").get(getAllPosts);
-router.route("/:id").put(updatePost);
+router.route("/:id").put(authUser, updatePost);
 router.route("/:id").get(getPostById);
-router.route("/:id").delete(deletePost);
+router.route("/:id").delete(authUser, deletePost);
 
 export default router;
